ac7eb1a46c4c2555a1324c827acedfaed87946f2
Alexej <door1975@yandex.ru>
2020-11-14 21:29:32 +0300

    Подправил класс MPVRunner. Добавил к построению ProseccBuilder вызов метода inheritIO. После этого, вроде бы, перестало все зависать через три-четыре минуты. Никак не могу создать исполняемый jar-файл.

--- src/main/java/YoutubePlayer/MPVRunner.java
+++ src/main/java/YoutubePlayer/MPVRunner.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class MPVRunner {
 
 	public void run(String videoId) {
-		ProcessBuilder processBuilder = new ProcessBuilder("mpv", "https://youtu.be/" + videoId);
+		ProcessBuilder processBuilder = new ProcessBuilder("mpv", "https://youtu.be/" + videoId).inheritIO();
 		try {
 			
 			processBuilder.start();
@@ -16,7 +16,7 @@ public class MPVRunner {
 	}
 
 	public void runBestFormat(String videoId) {
-		ProcessBuilder processBuilder = new ProcessBuilder("mpv","--ytdl-format=best", "https://youtu.be/" + videoId);
+		ProcessBuilder processBuilder = new ProcessBuilder("mpv","--ytdl-format=best", "https://youtu.be/" + videoId).inheritIO();
 		try {
 			
 			processBuilder.start();
@@ -24,6 +24,20 @@ public class MPVRunner {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		/*Runtime runtime = Runtime.getRuntime();
+		Process process = null;
+		try {
+			process = runtime.exec("mpv " + "--ytdl-format=best " + "https://youtu.be/" + videoId);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			process.waitFor();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 	}
 
 }
